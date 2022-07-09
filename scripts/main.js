c3f25c55c2318af747d8b314d21b21bb8865d22f
aidcaljone <aidcaljone@users.noreply.github.com>
2022-07-09 10:18:06 -0600

    k

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,11 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var imgs = document.querySelectorAll("gallery-img");
+const images = document.querySelectorAll(".gallery-img img");
+let imgSrc;
 
-var modalImg = document.getElementById("img01");
-var modalImg2 = document.getElementById("img02");
-var captionText = document.getElementById("caption");
-
-for (let i = 0; i<imgs.length; i++) {
-  imgs[i].onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    modalImg2.src = this.dataset.modal;
-    captionText.innerHTML = this.alt;
-  }
-
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// get images src onclick
+images.forEach((img) => {
+    img.addEventListener("click", (e) => {
+        imgSrc = e.target.src;
+        console.log(imgSrc)
+    });
+});
